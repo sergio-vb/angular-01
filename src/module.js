62e554e185e27cc1892b59d414362e3f2edc3f53
Svb home <sergio.valverde.b@gmail.com>
2017-06-04 17:30:49 -0600

    Testing isolate scope

--- src/module.js
+++ src/module.js
@@ -1,99 +1,159 @@
-		var demoApp = angular.module("demoApp", ["ngRoute"]); //[] Means no dependancies
+var demoApp = angular.module("demoApp", ["ngRoute"]); //[] Means no dependancies
 
-		demoApp.config(function($routeProvider){
-			$routeProvider
-				.when('/',{
-					controller: 'SimpleController',
-					templateUrl: 'src/partials/View1.html'
-				})
-				.when('/view2',{
-					controller: 'SimpleController',
-					templateUrl: 'src/partials/View2.html'
-				})
-				.otherwise({redirectTo: '/'});
-		});
+demoApp.config(function($routeProvider){
+	$routeProvider
+		.when('/',{
+			controller: 'SimpleController',
+			templateUrl: 'src/partials/View1.html'
+		})
+		.when('/view2',{
+			controller: 'SimpleController',
+			templateUrl: 'src/partials/View2.html'
+		})
+		.otherwise({redirectTo: '/'});
+});
 
-		demoApp.factory("simpleFactory", function(){
-			var factory = {},
-				customers = [
-					{
-						name: 'Dave Smith',
-						city: 'New York City'
-					},
-					{
-						name: 'Alex Doe',
-						city: 'Phoenix'
-					}
-				];
-
-			factory.getCustomers = function(){
-				console.log("Factory returned customers");
-				return customers;
+demoApp.factory("simpleFactory", function(){
+	var factory = {},
+		customers = [
+			{
+				name: 'Dave Smith',
+				city: 'New York City'
+			},
+			{
+				name: 'Alex Doe',
+				city: 'Phoenix'
 			}
-			return factory;
-		});
+		];
 
-		/*demoApp.service("simpleService", function(){
-			var customers = [
-					{
-						name: 'Dave Smith',
-						city: 'New York City'
-					},
-					{
-						name: 'Alex Doe',
-						city: 'Phoenix'
-					}
-				];
-				
-			this.getCustomers = function(){
-				console.log("Service returned customers");
-				return customers;
+	factory.getCustomers = function(){
+		console.log("Factory returned customers");
+		return customers;
+	}
+	return factory;
+});
+
+/*demoApp.service("simpleService", function(){
+	var customers = [
+			{
+				name: 'Dave Smith',
+				city: 'New York City'
+			},
+			{
+				name: 'Alex Doe',
+				city: 'Phoenix'
 			}
-		});*/
+		];
+		
+	this.getCustomers = function(){
+		console.log("Service returned customers");
+		return customers;
+	}
+});*/
 
-		demoApp.controller("SimpleController", function($scope, simpleFactory){
-			$scope.customers = simpleFactory.getCustomers();
+demoApp.controller("SimpleController", function($scope, simpleFactory){
+	$scope.customers = simpleFactory.getCustomers();
 
-			$scope.addCustomer = function(){
-				$scope.customers.push({
-					name: $scope.newCustomer.name,
-					city: $scope.newCustomer.city
-				});
-				console.log("Customer added");
-			};
-
+	$scope.addCustomer = function(){
+		$scope.customers.push({
+			name: $scope.newCustomer.name,
+			city: $scope.newCustomer.city
 		});
+		console.log("Customer added");
+	};
 
-		/* Could also be done as: 
-		var controllers = {};
-		controllers.SimpleController = function($scope){
-			$scope.customers = [
-				{
-					name: 'Dave Smith',
-					city: 'New York City'
-				},
-				{
-					name: 'Alex Doe',
-					city: 'Phoenix'
-				}
-			]
-		};
-		demoApp.controller(controllers); */
+});
 
-		demoApp.directive("testdirective", function(){
-			return {
-				restrict: 'E',
-				transclude: true,
-				link: function(scope, element, attributes){
-					
-					/*console.log("Scope: ", scope);
-					console.log("Element: ", element);
-					console.log("Attributes: ", attributes);*/
+/* Could also be done as: 
+var controllers = {};
+controllers.SimpleController = function($scope){
+	$scope.customers = [
+		{
+			name: 'Dave Smith',
+			city: 'New York City'
+		},
+		{
+			name: 'Alex Doe',
+			city: 'Phoenix'
+		}
+	]
+};
+demoApp.controller(controllers); */
 
-					element.bind("mouseenter", function(){
-						element[0].innerHTML = "They see me rollin'";
-					});
-				},
-				template: '<h2>Custom directive</h2>'
-			}
-		});
+demoApp.directive("testdirective", function(){
+	return {
+		restrict: 'E',
+		transclude: true,
+		link: function(scope, element, attributes){
+			
+			/*console.log("Scope: ", scope);
+			console.log("Element: ", element);
+			console.log("Attributes: ", attributes);*/
+
+			element.bind("mouseenter", function(){
+				element[0].innerHTML = "They see me rollin'";
+			});
+		},
+		template: '<h2>Custom directive</h2>'
+	}
+});
+
+//Example of an element directive using other attribute directives:
+demoApp.controller("AntariController", function($scope) {
+
+	$scope.antari = ["Antari 1"];
+
+	$scope.addKell = function() {
+		$scope.antari.push("Kell: Red London");
+	};
+
+	$scope.addLila = function() {
+		$scope.antari.push("Lila: Grey London");
+	};
+
+	$scope.addHolland = function() {
+		$scope.antari.push("Holland: White London");
+	};
+
+});
+
+//Creates a container directive with a set controller and scope
+demoApp.directive("antari", function(){
+	return{
+		restrict: "E",
+		controller: "AntariController",
+		scope: {},
+		link: function(scope, element, attributes){
+			element.bind("mouseenter", function(){
+				console.log("Antari: ", scope.antari);
+			})
+		}
+	}
+});
+
+//Creates attribute directives that can access the container directive's controller and therefore scope
+demoApp.directive("kell",function() {
+	return{
+		require: "antari",
+		link: function(scope, element, attributes, antariController) {
+			console.log("Scope in inner directive Kell", scope)
+			//console.log("Hello!");
+		}
+	}
+});
+/*demoApp.directive("lila",function() {
+	return{
+		require: "antari",
+		link: function(scope, element, attributes, antariController) {
+			console.log("Scope in inner directives: (Lila) ", scope)
+		}
+	}
+});*/
+/*demoApp.directive("holland",function() {
+	return{
+		require: "antari",
+		link: function(scope, element, attributes, antariController) {
+			//console.log("Scope in inner directives: (Holland) ", scope)
+		}
+	}
+});*/
